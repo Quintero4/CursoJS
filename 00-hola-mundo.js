672898c7dba16e4eebdded29a858c26b0d538e96
Fernando Quintero <fernandoquinteroprofesional@gmail.com>
2025-10-22 14:04:36 +0200

    react

---
+++ 00-hola-mundo.js
@@ -0,0 +1,22 @@
+import React from "https://esm.sh/react@18.2.0"
+
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+
+const appDomElement = document.getElementById('app')
+
+const root = ReactDOM.createRoot(appDomElement)
+
+const h = React.createElement ( )
+
+const button = h ('button', {"data-id": 123}, 'Me gusta 1')
+const button2 = h ('button', {"data-id": 456}, 'Me gusta 2')
+const button3 = h ('button', {"data-id": 789}, 'Me gusta 3')
+
+const app = h(React.Fragment, null, [button, button2, button3])
+
+
+/*<React.Fragment>
+<button> data-id="123" Button1 </button>
+<button> data-id="456" Button1 </button>
+<button> data-id="789" Button1 </button>
+*/
